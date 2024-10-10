dd210fde1b7c42957ee7e85cbdba897d00184419
Tove Karlström <tovekarlstrom@hotmail.se>
2024-10-10 16:55:46 +0200

    feat: new styling for enter question page

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,13 +1,17 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet } from "react-native";
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
 
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { AddQuestion } from "@/components/AddQuestion";
 import { AddedQuestions } from "@/components/AddedQuestions";
+import { Colors } from "@/constants/Theme";
+import { useState } from "react";
+import { CardComponent } from "@/components/CardComponent";
 
 export default function TabThreeScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
@@ -17,12 +21,17 @@ export default function TabThreeScreen() {
     >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="heading32">
-          Enter questions to challenge the crowd
+          Enter <Text style={styles.titleSpan}>questions</Text> to challenge the
+          crowd
         </ThemedText>
       </ThemedView>
-      <ThemedText>Add content</ThemedText>
       <AddQuestion />
-      <AddedQuestions />
+      <CardComponent heading="Added Questions">
+        <AddedQuestions />
+      </CardComponent>
+      <CardComponent heading="Added Questions">
+        <ThemedText type="heading32">Enter</ThemedText>
+      </CardComponent>
     </ParallaxScrollView>
   );
 }
@@ -37,5 +46,15 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: "row",
     gap: 8,
+    marginBottom: 70,
+  },
+  titleSpan: {
+    color: Colors.light.contrastBlue,
+  },
+  card: {
+    marginTop: 50,
+    backgroundColor: Colors.light.Card,
+    borderRadius: 20,
+    padding: 15,
   },
 });
--- components/AddedQuestions.tsx
+++ components/AddedQuestions.tsx
@@ -19,5 +19,6 @@ const styled = StyleSheet.create({
   container: {
     flexDirection: "column",
     gap: Sizes.Spacings.medium,
+    flex: 1,
   },
 });

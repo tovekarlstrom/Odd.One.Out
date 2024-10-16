0b338526cc01f5a6544e70337f6341d1f67ee2a5
Tove Karlström <tovekarlstrom@hotmail.se>
2024-10-16 09:41:58 +0200

    feat: add text field to joined players

--- components/JoinedPlayers.tsx
+++ components/JoinedPlayers.tsx
@@ -1,10 +1,13 @@
 import { CardComponent } from "./CardComponent";
 import PlayerIcon from "./PlayerIcon";
+import { TextField } from "./TextField";
 
 export function JoinedPlayers() {
   return (
     <CardComponent heading="Joined Players">
-      <PlayerIcon />
+      <TextField value="Klara">
+        <PlayerIcon size={20} />
+      </TextField>
     </CardComponent>
   );
 }
